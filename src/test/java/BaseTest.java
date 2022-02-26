68f1ff6d72bbd0cf96a6223b28873c6c59df5c3d
Pasha Shalunov <p.shalunov@iteco-inno.ru>
2022-02-26 21:57:40 +0400

    first commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
