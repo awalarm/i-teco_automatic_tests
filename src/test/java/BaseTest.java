cb2b78cc12d609de29a3dc545d19cf7e53521b24
Pasha Shalunov <p.shalunov@iteco-inno.ru>
2022-02-26 22:59:48 +0400

    Creating test "User Login"

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,5 +20,18 @@ public class BaseTest {
     @AfterEach
     void teardown() {
         driver.quit();
+    }
+
+    @Test
+    @DisplayName("Авторизация пользователя")
+    void test1() {
+        driver.navigate().to("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//i[contains(text(),'Login')]")).click();
+
+        Assertions.assertEquals(driver.findElement(By.xpath("//h2[contains(text(),'Secure Area')]")).getText(), "Secure Area");
     }
 }
